ec9f7c58e4ab08c8a650febb7c727225fdb0e282
v-rapha <vs.raphael19@gmail.com>
2021-08-26 20:45:37 -0300

    feat: add css, sass, postcss, hotreloading, browserslist

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './styles/index.scss';
+
 const someObj = {
   foo: 'bar',
   bar() {
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,25 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 let mode = 'development';
 
 if (process.env.NODE_ENV === 'production') {
   mode = 'production';
 }
 
-
 module.exports = {
   mode: mode,
 
   module: {
     rules: [
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
@@ -20,8 +30,11 @@ module.exports = {
     ],
   },
 
+  plugins: [new MiniCssExtractPlugin()],
+
   devtool: 'source-map',
   devServer: {
     static: './dist',
+    hot: true,
   },
 };
